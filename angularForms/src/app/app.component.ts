0ec0d1e1841e73181f08551ee62d3c0cdcd410cc
saranya <saranya@tangledai.com>
2021-06-30 11:44:33 +0530

    set default value for all fields

--- angularForms/src/app/app.component.ts
+++ angularForms/src/app/app.component.ts
@@ -4,21 +4,30 @@ import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  @ViewChild('form') signupFrom:NgForm;
+  @ViewChild('form') signupFrom: NgForm;
   title = 'angularForms';
-  defaultValue="pet";
-  answer:string = '';
-  gender:Array<string> = ['male','female'];
+  defaultValue = 'pet';
+  answer: string = '';
+  gender: Array<string> = ['male', 'female'];
   suggestUserName() {
     const suggestedName = 'Superuser';
+    this.signupFrom.setValue({
+      userData: {
+        username: 'SaranyaG',
+        email: 'ugomecome@gmail.com',
+        question: 'lkkkll',
+        secret: 'pet',
+        gender: 'female',
+      },
+    });
   }
   // onSubmit(form:NgForm){
   //   console.log("form submitted!!",form);
   // }
-  onSubmit(){
+  onSubmit() {
     console.log(this.signupFrom);
   }
 }
