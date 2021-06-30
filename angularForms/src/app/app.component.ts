35ef492b5d936b4760955005e62f8d749797ed1a
saranya <saranya@tangledai.com>
2021-06-30 14:19:20 +0530

    sumitted form

--- angularForms/src/app/app.component.ts
+++ angularForms/src/app/app.component.ts
@@ -12,6 +12,13 @@ export class AppComponent {
   defaultValue = 'pet';
   answer: string = '';
   gender: Array<string> = ['male', 'female'];
+  userData= {
+        username: '',
+        email: '',
+        question: '',
+        secret: 'pet',
+        gender: 'female',
+      };
   suggestUserName() {
     const suggestedName = 'Superuser';
     // this.signupFrom.setValue({
@@ -23,11 +30,24 @@ export class AppComponent {
     //     gender: 'female',
     //   },
     // });
+    this.signupFrom.form.patchValue({
+      userData: {
+        username: 'SaranyaG',
+        email: 'ugomecome@gmail.com',
+        question: 'lkkkll',
+        secret: 'pet',
+        gender: 'female',
+      },
+    })
   }
   // onSubmit(form:NgForm){
   //   console.log("form submitted!!",form);
   // }
   onSubmit() {
     console.log(this.signupFrom);
+    this.userData.username = this.signupFrom.value.userData.username;
+    this.userData.email = this.signupFrom.value.userData.email;
+    this.userData.gender =  this.signupFrom.value.userData.gender;
+    this.userData.question = this.signupFrom.value.userData.question;
   }
 }
